dff8500594f561d72b57c6c5ebc258e2c9a983a9
dglusak <dglusak@gmail.com>
2023-07-18 14:12:31 -0400

    Update Character.cs

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -8,7 +8,7 @@ public class Character : MonoBehaviour
 
     private void Update()
     {
-        Vector2 diff = _moveInput * rootObject.Speed;
+        Vector2 diff = _moveInput * rootObject.Speed * Time.deltaTime;
         rootObject.transform.position = (Vector3)diff;
     }
 
